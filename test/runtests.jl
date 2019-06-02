3c189f7a67d4897a8769df82ede92addc1d10562
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2019-06-02 21:57:55 +0200

    Restrict SaferIntegers tests to Julia > 1.0.4

--- test/runtests.jl
+++ test/runtests.jl
@@ -1424,6 +1424,29 @@ end
         end
     end
 
+    @testset "flipsign" begin
+        for T in (halfinttypes..., :BigHalfInt)
+            @eval @test @inferred(flipsign($T(3/2), -1)) isa $T
+            @eval @test @inferred(flipsign($T(3/2), 1.0)) isa $T
+            @eval @test @inferred(flipsign($T(3/2), $T(1))) isa $T
+            @eval @test flipsign($T(0), $T(-3/2)) == 0
+            @eval @test flipsign($T(0), 4) == 0
+            @eval @test flipsign($T(11/2), $T(-7/2)) == -11/2
+            @eval @test flipsign($T(11/2), 5.0) == 11/2
+            @eval @test flipsign($T(-1/2), $T(3/2)) == -1/2
+            @eval @test flipsign($T(-1/2), -1//3) == 1/2
+        end
+        for T in halfuinttypes
+            @eval @test @inferred(flipsign($T(3/2), 5)) isa $T
+            @eval @test @inferred(flipsign($T(3/2), 1.0)) isa $T
+            @eval @test @inferred(flipsign($T(3/2), $T(1))) isa $T
+            @eval @test flipsign($T(0), $T(3/2)) == 0
+            @eval @test flipsign($T(0), -5) == 0
+            @eval @test flipsign($T(11/2), 1//3) == 11/2
+            @eval @test flipsign($T(11/2), 0.0) == 11/2
+        end
+    end
+
     @testset "Complex" begin
         for T in (halfinttypes..., halfuinttypes..., :BigHalfInt)
             @eval @test $T(9/2)*im isa Complex{$T}
@@ -2023,53 +2046,61 @@ end
     end
 end
 
-using SaferIntegers
+@static if VERSION > v"1.0.4"
+    # In Julia ≤ 1.0.4, zero(Half{T}) and one(Half{T}) do not work for
+    # T ∈ [SafeInt8, SafeInt16, SafeUInt8, SafeUInt16] due to a bug in Julia, cf.
+    # https://github.com/JuliaLang/julia/issues/32203
 
-safeinttypes  = (:SafeInt, :SafeInt8, :SafeInt16, :SafeInt32, :SafeInt64, :SafeInt128)
-safeuinttypes = (:SafeUInt, :SafeUInt8, :SafeUInt16, :SafeUInt32, :SafeUInt64, :SafeUInt128)
+    using SaferIntegers
 
-@testset "SaferIntegers" begin
-    @test_throws OverflowError Half{SafeInt8}(typemax(Int8))
-    @test_throws OverflowError typemax(Half{SafeInt8}) + one(HalfInt8)
-    @test_throws OverflowError typemax(Half{SafeInt8}) + one(Int8)
-    @test_throws OverflowError typemin(Half{SafeInt8}) - one(HalfInt8)
-    @test_throws OverflowError typemin(Half{SafeInt8}) - one(Int8)
-    for T in safeinttypes
-        @eval @test HalfInteger(one($T)) === one(Half{$T})
-        @eval @test_throws OverflowError Half{$T}(typemax($T))
-        @eval @test_throws OverflowError Half{$T}(typemin($T))
-        @eval @test_throws OverflowError Half{$T}(typemax($T)//one($T))
-        @eval @test_throws OverflowError Half{$T}(typemin($T)//one($T))
-        @eval @test_throws OverflowError Half{$T}(complex(typemax($T)))
-        @eval @test_throws OverflowError Half{$T}(complex(typemin($T)))
-        @eval @test_throws OverflowError Half{$T}(complex(typemax($T)//one($T)))
-        @eval @test_throws OverflowError Half{$T}(complex(typemin($T)//one($T)))
-        @eval @test_throws OverflowError -typemin(Half{$T})
-        @eval @test_throws OverflowError abs(typemin(Half{$T}))
-        @eval @test_throws OverflowError typemax(Half{$T}) + onehalf(Half{$T})
-        @eval @test_throws OverflowError typemin(Half{$T}) - onehalf(Half{$T})
-        @eval @test_throws OverflowError twice(typemax($T))
-        @eval @test_throws OverflowError twice(typemin($T))
-        @eval @test_throws OverflowError round(typemax(Half{$T}))
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearest)
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesAway)
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesUp)
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundUp)
-    end
-    for T in safeuinttypes
-        @eval @test HalfInteger(one($T)) === one(Half{$T})
-        @eval @test_throws OverflowError Half{$T}(typemax($T))
-        @eval @test_throws OverflowError Half{$T}(typemax($T)//one($T))
-        @eval @test_throws OverflowError Half{$T}(complex(typemax($T)))
-        @eval @test_throws OverflowError Half{$T}(complex(typemax($T)//one($T)))
-        @eval @test_throws OverflowError -onehalf(Half{$T})
-        @eval @test_throws OverflowError typemax(Half{$T}) + onehalf(Half{$T})
-        @eval @test_throws OverflowError typemin(Half{$T}) - onehalf(Half{$T})
-        @eval @test_throws OverflowError twice(typemax($T))
-        @eval @test_throws OverflowError round(typemax(Half{$T}))
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearest)
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesAway)
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesUp)
-        @eval @test_throws OverflowError round(typemax(Half{$T}), RoundUp)
+    safeinttypes  = (:SafeInt, :SafeInt8, :SafeInt16, :SafeInt32, :SafeInt64, :SafeInt128)
+    safeuinttypes = (:SafeUInt, :SafeUInt8, :SafeUInt16, :SafeUInt32, :SafeUInt64, :SafeUInt128)
+
+    @testset "SaferIntegers" begin
+        @test_throws OverflowError Half{SafeInt8}(typemax(Int8))
+        @test_throws OverflowError typemax(Half{SafeInt8}) + one(HalfInt8)
+        @test_throws OverflowError typemax(Half{SafeInt8}) + one(Int8)
+        @test_throws OverflowError typemin(Half{SafeInt8}) - one(HalfInt8)
+        @test_throws OverflowError typemin(Half{SafeInt8}) - one(Int8)
+        for T in safeinttypes
+            @eval @test HalfInteger(one($T)) === one(Half{$T})
+            @eval @test_throws OverflowError Half{$T}(typemax($T))
+            @eval @test_throws OverflowError Half{$T}(typemin($T))
+            @eval @test_throws OverflowError Half{$T}(typemax($T)//one($T))
+            @eval @test_throws OverflowError Half{$T}(typemin($T)//one($T))
+            @eval @test_throws OverflowError Half{$T}(complex(typemax($T)))
+            @eval @test_throws OverflowError Half{$T}(complex(typemin($T)))
+            @eval @test_throws OverflowError Half{$T}(complex(typemax($T)//one($T)))
+            @eval @test_throws OverflowError Half{$T}(complex(typemin($T)//one($T)))
+            @eval @test_throws OverflowError -typemin(Half{$T})
+            @eval @test_throws OverflowError abs(typemin(Half{$T}))
+            @eval @test_throws OverflowError flipsign(typemin(Half{$T}), -1)
+            @eval @test_throws OverflowError typemax(Half{$T}) + onehalf(Half{$T})
+            @eval @test_throws OverflowError typemin(Half{$T}) - onehalf(Half{$T})
+            @eval @test_throws OverflowError twice(typemax($T))
+            @eval @test_throws OverflowError twice(typemin($T))
+            @eval @test_throws OverflowError round(typemax(Half{$T}))
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearest)
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesAway)
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesUp)
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundUp)
+        end
+        for T in safeuinttypes
+            @eval @test HalfInteger(one($T)) === one(Half{$T})
+            @eval @test_throws OverflowError Half{$T}(typemax($T))
+            @eval @test_throws OverflowError Half{$T}(typemax($T)//one($T))
+            @eval @test_throws OverflowError Half{$T}(complex(typemax($T)))
+            @eval @test_throws OverflowError Half{$T}(complex(typemax($T)//one($T)))
+            @eval @test_throws OverflowError -onehalf(Half{$T})
+            @eval @test_throws OverflowError flipsign(onehalf(Half{$T}), -1)
+            @eval @test_throws OverflowError typemax(Half{$T}) + onehalf(Half{$T})
+            @eval @test_throws OverflowError typemin(Half{$T}) - onehalf(Half{$T})
+            @eval @test_throws OverflowError twice(typemax($T))
+            @eval @test_throws OverflowError round(typemax(Half{$T}))
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearest)
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesAway)
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundNearestTiesUp)
+            @eval @test_throws OverflowError round(typemax(Half{$T}), RoundUp)
+        end
     end
 end
