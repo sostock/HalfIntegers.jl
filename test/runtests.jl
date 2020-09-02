3de27e1c9dc6e3e60e2160184120c2baa776058a
Sebastian Stock <42280794+sostock@users.noreply.github.com>
2020-09-02 17:09:52 +0200

    Fix ambiguity detection on master (#23)

--- test/runtests.jl
+++ test/runtests.jl
@@ -9,7 +9,11 @@ halfuinttypes = (:HalfUInt8, :HalfUInt16, :HalfUInt32, :HalfUInt64, :HalfUInt128
 ==ₜ(x::T, y::T) where T<:Union{BigInt,BigFloat,BigHalfInt,Rational{BigInt},Complex{BigInt},Complex{BigFloat},Complex{BigHalfInt},Complex{Rational{BigInt}}} = x == y
 ==ₜ(x::AbstractArray, y::AbstractArray) = (x == y) && (typeof(x) == typeof(y))
 
-@test isempty(Test.detect_ambiguities(HalfIntegers, Base, Core))
+@static if VERSION ≥ v"1.6.0-DEV.816"
+    @test isempty(Test.detect_ambiguities(HalfIntegers))
+else
+    @test isempty(Test.detect_ambiguities(HalfIntegers, Base, Core))
+end
 
 @testset "Aliases" begin
     @test HalfInt === Half{Int}
